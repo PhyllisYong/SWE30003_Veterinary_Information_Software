65c463d8f8873ca923a4e0ed352eb9ac255da730
NatalieRobert <natalie050127@gmail.com>
2026-05-27 23:04:20 +0800

    feat : Video page

--- vet_frontend/src/App.tsx
+++ vet_frontend/src/App.tsx
@@ -9,6 +9,9 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 
 import FirstAidGuides from './pages/FirstAidPage'
+import Video from './pages/VideoPage'
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -20,6 +23,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/guides" element={<FirstAidGuides />} />
+        <Route path="/videos" element={<Video />} />
       </Routes>
       <Footer />
     </BrowserRouter>
